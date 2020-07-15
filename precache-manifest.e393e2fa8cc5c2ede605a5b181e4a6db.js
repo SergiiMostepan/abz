b32fba6ebe1d73424f1d6d7660c73a2a7da1e263
Sergii <sergiimostepan@gmail.com>
2020-07-15 18:50:34 +0300

    Updates

---
+++ precache-manifest.e393e2fa8cc5c2ede605a5b181e4a6db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3bf2744c86b8fdf2b8723f139e06591",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "8a8e3e42a6c617b0ad21",
+    "url": "/my-app/static/css/main.126e3739.chunk.css"
+  },
+  {
+    "revision": "600b0335938e14665f3f",
+    "url": "/my-app/static/js/2.b7a20b65.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/my-app/static/js/2.b7a20b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8e3e42a6c617b0ad21",
+    "url": "/my-app/static/js/main.ca3d200b.chunk.js"
+  },
+  {
+    "revision": "1a38bed923dbef64b85c",
+    "url": "/my-app/static/js/runtime-main.ef42851e.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/my-app/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/my-app/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/my-app/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/my-app/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
